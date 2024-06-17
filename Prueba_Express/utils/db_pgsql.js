6add34084b0bb20d7d4eb6f22114eddad7193539
Nitolez <antonioglezt98@gmail.com>
2024-06-17 19:37:56 +0200

    Terminado

---
+++ Prueba_Express/utils/db_pgsql.js
@@ -0,0 +1,9 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+    host: 'localhost',
+    user: 'postgres',
+    port: '5432',
+    database: 'postgres',
+    password: '123456-d'
+});
